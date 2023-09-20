ede7b6aa4864fe9af01fe159f426eb288baef8d5
jakelipscomb <jlipscombtx95@gmail.com>
2023-09-19 22:11:39 -0500

    fixed install.js to handle install button

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,13 +15,9 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
-
   const tx = jateDb.transaction('jate', 'readwrite');
-
   const store = tx.objectStore('jate');
-
   const request = store.put({ id: 1, jate: content });
-
   await request;
 }
 console.error('putDb not implemented');
@@ -29,13 +25,9 @@ console.error('putDb not implemented');
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
 const jateDb = await openDB('jate', 1);
-
 const tx = jateDb.transaction('jate', 'readonly');
-
 const store = tx.objectStore('jate');
-
 const request = store.get(1);
-
 const result = await request;
 
 return result?.value;
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,14 +1,28 @@
 const butInstall = document.getElementById('buttonInstall');
 
+let installPrompt;
+
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
+    window.installPrompt = event;
     butInstall.style.visibility = 'visible';
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.installPrompt;
+    if (promptEvent) {
+        promptEvent.prompt();
+        window.installPrompt = null;
+        butInstall.style.visibility = 'visible';
+    }
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.installPrompt = null;
+    console.log('App installed successfully')
+});
+
